34b6ab65cf63e10bdbf05ef4538def6340a4c524
Yash Umale <yashumale223@gmail.com>
2021-02-08 19:35:00 +0530

    LoginActivity changes - 1

--- app/src/main/java/com/openlab/humanpokedex/LoginActivity.java
+++ app/src/main/java/com/openlab/humanpokedex/LoginActivity.java
@@ -1,4 +1,18 @@
 package com.openlab.humanpokedex;
 
-public class LoginActivity {
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.login_activity);
+    }
+
+
 }
