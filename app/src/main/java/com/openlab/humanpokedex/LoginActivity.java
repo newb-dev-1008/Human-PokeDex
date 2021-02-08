cb52c493c833f63d41911ccf5a2f5a511661250b
Yash Umale <yashumale223@gmail.com>
2021-02-08 19:47:29 +0530

    LoginActivity changes - 2

--- app/src/main/java/com/openlab/humanpokedex/LoginActivity.java
+++ app/src/main/java/com/openlab/humanpokedex/LoginActivity.java
@@ -6,13 +6,19 @@ import android.os.PersistableBundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.button.MaterialButton;
+
 public class LoginActivity extends AppCompatActivity {
+
+    private MaterialButton signIn, signUp, recFace;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
         super.onCreate(savedInstanceState, persistentState);
         setContentView(R.layout.login_activity);
-    }
 
+        signIn = findViewById(R.id.signInButton);
+        signUp = findViewById(R.id.signUpButton);
+    }
 
 }
