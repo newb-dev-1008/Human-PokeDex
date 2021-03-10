c2d92e17fa34172d876a0bbff18b223662a94dde
Yash Umale <yashumale223@gmail.com>
2021-03-10 21:28:59 +0530

    Face registration Java - 4

--- app/src/main/java/com/openlab/humanpokedex/RegisterFaceActivity.java
+++ app/src/main/java/com/openlab/humanpokedex/RegisterFaceActivity.java
@@ -7,6 +7,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class RegisterFaceActivity extends AppCompatActivity {
 
+    private int REQUEST_CODE_PERMISSIONS = 1001;
+    private final String[] REQUIRED_PERMISSIONS = new String[]{"android.permission.CAMERA", "android.permission.WRITE_EXTERNAL_STORAGE"};
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
