7aed0c5fa457974bc840a56b5c2c258df626424a
Yash Umale <yashumale223@gmail.com>
2021-03-17 16:55:22 +0530

    Augment data - 1

--- Python Files/Project Files/opencv-face-recognition/augment_data.py
+++ Python Files/Project Files/opencv-face-recognition/augment_data.py
@@ -1,7 +1,7 @@
 import numpy as np
 import Augmentor
 
-imagePath = "https://console.firebase.google.com/project/human-pokedex/storage/human-pokedex.appspot.com/files"
+imagePaths = list(paths.list_images("C:\\Users\\Yash Umale\\Documents\\6th Sem\\Open Lab\\Python Files\\Project Files\\New Datasets"))
 
 for imagePath in imagePaths:
     name = imagePath.split(os.path.sep)[-2]
@@ -9,38 +9,38 @@ for imagePath in imagePaths:
     # Perform data augmentation
     blurImage(imagePath)
     sharpenImage(imagePath)
-
+    sepiaImage()
 
 # ---------------------------- Functions for data augmentation ----------------------------
 
 # Function to blur the image
-def blurImage(image):
+def blurImage(imagePath):
     blurredImage = gaussianBlur(copy.deepcopy(image))
     filename = "blurred_" + imagePath.split(os.path.sep)[-1] + ".jpg"
     cv2.imwrite(filename, blurredImage))
 
 # Function to sharpen the image
-def sharpenImage(image):
+def sharpenImage(imagePath):
     kernel = np.array([[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]])
     sharpImage = cv2.filter2D(image, -1, kernel)
     filename = "sharpened_" + imagePath.split(os.path.sep)[-1] + ".jpg"
     cv2.imwrite(filename, sharpImage)
 
 # Function to add Sepia effect 
-def sepiaImage(image):
+def sepiaImage(imagePath):
     kernel = np.array([[0.272, 0.534, 0.131], [0.349, 0.686, 0.168], [0.393, 0.769, 0.189]])
     sepiaImage = cv2.filter2D(image, -1, kernel)
     filename = "sepia_" + imagePath.split(os.path.sep)[-1] + ".jpg"
     cv2.imwrite(filename, sharpImage)
 
 # Function to add brightness
-def brightImage(image):
+def brightImage(imagePath):
     brightImage = cv2.convertScaleAbs(image, 3)
     filename = "bright_" + imagePath.split(os.path.sep)[-1] + ".jpg"
     cv2.imwrite(filename, brightImage)
 
 # Tilt image to certain angles
-def tiltedImage(image, imagePath):
+def tiltedImage(imagePath):
     p = Augmentor.Pipeline(imagePath)
     p.rotate(probability = 1, max_left_rotate = 15, max_right_rotate = 15)
     tilted_images, label = p.sample(10)
@@ -49,17 +49,15 @@ def tiltedImage(image, imagePath):
         cv2.imwrite(filename, tilted_images[i])
     
 # Mirror image
-def mirrorImage(image, imagePath):
+def mirrorImage(imagePath):
     p = Augmentor.Pipeline(imagePath)
     p.flip_left_right(probability = 1)
     flipped_images, label = p.sample(1)
     filename = "mirror_" + imagePath.split(os.path.sep)[-1] + ".jpg"
     cv2.imwrite(filename, flipped_images[0])
 
-# Dull the image
-
 # Shearing image
-def shearImage(image, imagePath):
+def shearImage(imagePath):
     p = Augmentor.Pipeline(imagePath)
     p.shear(probability = 1, 15, 15)
     sheared_images, label = p.sample(10)
@@ -68,7 +66,7 @@ def shearImage(image, imagePath):
         cv2.imwrite(filename, sheared_images[i])
 
 # Skewing image
-def skewedImage(image, imagePath):
+def skewedImage(imagePath):
     p = Augmentor.Pipeline(imagePath)
     p.skew(probability = 1, magnitude = 0.7)
     skewed_images, label = p.sample(10)
@@ -77,7 +75,7 @@ def skewedImage(image, imagePath):
         cv2.imwrite(filename, skewed_images[i])
 
 # Black and White 
-def bwImage(image, imagePath):
+def bwImage(imagePath):
     p = Augmentor.Pipeline(imagePath)
     p.black_and_white(probability = 1, threshold = 256)
     bwImages, label = p.sample(1)
