1e6d20049af2b754249a995689b8c0a66986848c
Yash Umale <yashumale223@gmail.com>
2021-04-03 20:58:40 +0530

    Adding new extra details layout - 3

--- app/src/main/java/com/openlab/humanpokedex/AddDetailsActivity.java
+++ app/src/main/java/com/openlab/humanpokedex/AddDetailsActivity.java
@@ -22,6 +22,14 @@ public class AddDetailsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.extra_details);
 
-
+        nameET = findViewById(R.id.extraNameET);
+        classET = findViewById(R.id.extraClassET);
+        phoneET = findViewById(R.id.extraPhoneET);
+        deptET = findViewById(R.id.extraDeptET);
+        yearET = findViewById(R.id.extraYearET);
+        emailET = findViewById(R.id.extraEmailET);
+        registerBtn = findViewById(R.id.extraRegisterBtn);
+        registerProgress = findViewById(R.id.extraProgress);
+        registerTV = findViewById(R.id.extraTV);
     }
 }
