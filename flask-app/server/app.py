146abee5bf968f090cc05aa7458ec01baf6abd95
Yash Umale <yashumale223@gmail.com>
2021-04-05 10:50:55 +0530

    Added .py files for Flask

--- flask-app/server/app.py
+++ flask-app/server/app.py
@@ -1,11 +1,13 @@
 from flask import Flask
 import os
+from 
 
 app = Flask(__name__)
 @app.route('/')
 
 def index():
     # Complete this 
+
 
 if __name__ == '__main__':
     app.run(debug = True, host = '0.0.0.0', port = int(os.environ.get('PORT', 8080)))
