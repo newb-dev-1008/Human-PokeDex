7506296151503fb5a36daf2304934d4a5404192c
Yash Umale <yashumale223@gmail.com>
2021-04-05 10:18:49 +0530

    Splashscreen Java - 2

--- app/src/main/java/com/openlab/humanpokedex/SplashScreenActivity.java
+++ app/src/main/java/com/openlab/humanpokedex/SplashScreenActivity.java
@@ -1,11 +1,13 @@
 package com.openlab.humanpokedex;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
@@ -17,5 +19,22 @@ public class SplashScreenActivity extends AppCompatActivity {
         setContentView(R.layout.splash_screen);
 
         firebaseAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        if (user != null) {
+            Intent intent = new Intent(SplashScreenActivity.this, HomeActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+            finish();
+        } else {
+            Intent intent = new Intent(SplashScreenActivity.this, LoginSignUpActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+            finish();
+        }
     }
 }
