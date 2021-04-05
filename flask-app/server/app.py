0a62065d3f47c465fbc26fd4c850c7835e750d9c
Yash Umale <yashumale223@gmail.com>
2021-04-05 10:27:56 +0530

    Flask app - 2

--- flask-app/server/app.py
+++ flask-app/server/app.py
@@ -1,2 +1,11 @@
 from flask import Flask
+import os
 
+app = Flask(__name__)
+@app.route('/')
+
+def index():
+    # Complete this 
+
+if __name__ == '__main__':
+    app.run(debug = True, host = '0.0.0.0', port = int(os.environ.get('PORT', 8080)))
