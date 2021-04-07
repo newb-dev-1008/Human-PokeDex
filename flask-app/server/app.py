2690dc474bc78b9fdf464a0be2a502cb542c5ff4
Yash Umale <yashumale223@gmail.com>
2021-04-07 09:18:34 +0530

    Splashscreen Java - 3

--- flask-app/server/app.py
+++ flask-app/server/app.py
@@ -6,6 +6,16 @@ from pythonfiles import extract_embeddings
 app = Flask(__name__)
 @app.route('/')
 
+config = {
+    apiKey: "AIzaSyBqFROlkrLs0fMirkUoV4Sutn8AkTlBPlQ",
+    authDomain: "human-pokedex.firebaseapp.com",
+    projectId: "human-pokedex",
+    storageBucket: "human-pokedex.appspot.com",
+    messagingSenderId: "466324270281",
+    appId: "1:466324270281:web:d61e64e5c20932db15b118",
+    measurementId: "G-V0DRGWJ51J"
+}
+
 def index():
     # Complete this
     augment_data.mainAugment()
