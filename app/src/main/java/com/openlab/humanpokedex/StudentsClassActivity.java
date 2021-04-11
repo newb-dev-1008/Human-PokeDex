38e720f7d1a23771265700563c8fa942154839ce
Yash Umale <yashumale223@gmail.com>
2021-04-11 14:38:15 +0530

    Main Activity for displaying students - Adapter linked

--- app/src/main/java/com/openlab/humanpokedex/StudentsClassActivity.java
+++ app/src/main/java/com/openlab/humanpokedex/StudentsClassActivity.java
@@ -1,15 +1,26 @@
 package com.openlab.humanpokedex;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class StudentsClassActivity extends AppCompatActivity {
@@ -19,17 +30,26 @@ public class StudentsClassActivity extends AppCompatActivity {
     private TextView classnameTV;
     private ArrayList<ClassStudents> classStudents;
     private RecyclerView recyclerView;
+    private String className, dept;
+
+    // private RecyclerView.LayoutManager classStudentsLayoutManager;
+    private RecyclerView.Adapter classStudentsAdapter;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.class_students);
 
+        className = getIntent().getStringExtra("className");
+        dept = getIntent().getStringExtra("dept");
+
         db = FirebaseFirestore.getInstance();
         swipeRefreshLayout = findViewById(R.id.swipeRefreshClassStudents);
         classnameTV = findViewById(R.id.classNameTV);
         recyclerView = findViewById(R.id.class_studentsRecycler);
         classStudents = new ArrayList<ClassStudents>();
+
+        classnameTV.setText(className);
 
         swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
@@ -38,5 +58,44 @@ public class StudentsClassActivity extends AppCompatActivity {
                 swipeRefreshLayout.setRefreshing(false);
             }
         });
+    }
+
+    private void showStudents() {
+        db.collection("Campus").document("Department " + dept)
+                .collection("Class " + className).get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+            @Override
+            public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                if (queryDocumentSnapshots.size() > 0) {
+                    ArrayList<ClassStudents> classStudents = new ArrayList<>();
+                    for (DocumentSnapshot documentSnapshot : queryDocumentSnapshots) {
+                        String name = documentSnapshot.get("Username").toString();
+                        String regNo = documentSnapshot.get("RegNo").toString();
+                        String photoStoredStr = documentSnapshot.get("photoStored").toString();
+
+                        try {
+                            URI uri = new URI(photoStoredStr);
+                            URL photoStored = uri.toURL();
+                            classStudents.add(new ClassStudents(regNo, name, className, photoStored));
+                        } catch (URISyntaxException | MalformedURLException e) {
+                            Toast.makeText(StudentsClassActivity.this, "Unable to parse URI.", Toast.LENGTH_SHORT).show();
+                            finish();
+                        }
+
+                    }
+                }
+
+                if (classStudents.size() == 0) {
+                    classnameTV.setText("Empty Class.");
+                } else {
+                    GridLayoutManager classStudentsLayoutManager = new GridLayoutManager(StudentsClassActivity.this, 3);
+                    classStudentsAdapter = new StudentsClassAdapter(classStudents);
+                    recyclerView.setHasFixedSize(true);
+                    recyclerView.setLayoutManager(classStudentsLayoutManager);
+                    recyclerView.setAdapter(classStudentsAdapter);
+                    // swipeDownRefreshTV.setVisibility(View.GONE);
+                }
+
+            }
+        })
     }
 }
