741498e9b8d5445c3d4c6c885b2a9def1012046c
Yash Umale <yashumale223@gmail.com>
2021-04-11 11:17:21 +0530

    Java for displaying students - 2

---
+++ app/src/main/java/com/openlab/humanpokedex/ClassStudents.java
@@ -0,0 +1,31 @@
+package com.openlab.humanpokedex;
+
+import android.graphics.Bitmap;
+
+public class ClassStudents {
+
+    private String regNo, name, className, photosURL;
+
+    public ClassStudents(String regNo, String name, String className, String photosURL) {
+        this.regNo = regNo;
+        this.name = name;
+        this.className = className;
+        this.photosURL = photosURL;
+    }
+
+    public String getRegNo() {
+        return regNo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public String getPhotosURL() {
+        return photosURL;
+    }
+}
