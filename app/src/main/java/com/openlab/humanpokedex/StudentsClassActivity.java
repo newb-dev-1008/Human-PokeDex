537de257230a14540ae68d2de85900d0d261cd03
Yash Umale <yashumale223@gmail.com>
2021-04-11 11:15:44 +0530

    Java for displaying students - 1

---
+++ app/src/main/java/com/openlab/humanpokedex/StudentsClassActivity.java
@@ -0,0 +1,34 @@
+package com.openlab.humanpokedex;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+
+public class StudentsClassActivity extends AppCompatActivity {
+
+    private FirebaseFirestore db;
+    private SwipeRefreshLayout swipeRefreshLayout;
+    private TextView classnameTV;
+    private ArrayList<ClassStudents> classStudents;
+    private RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.class_students);
+
+        db = FirebaseFirestore.getInstance();
+        swipeRefreshLayout = findViewById(R.id.swipeRefreshClassStudents);
+        classnameTV = findViewById(R.id.classNameTV);
+        recyclerView = findViewById(R.id.class_studentsRecycler);
+        classStudents = new ArrayList<ClassStudents>();
+    }
+}
