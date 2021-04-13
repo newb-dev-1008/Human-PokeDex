f77595943ecbe35509678ed58509aecf92f3a7ee
Yash Umale <yashumale223@gmail.com>
2021-04-13 21:24:12 +0530

    Face detector added to project

---
+++ app/src/main/java/com/openlab/humanpokedex/DetectFaceActivity.java
@@ -0,0 +1,19 @@
+package com.openlab.humanpokedex;
+
+import android.media.FaceDetector;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DetectFaceActivity extends AppCompatActivity {
+
+    private FaceDetector faceDetector;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+}
