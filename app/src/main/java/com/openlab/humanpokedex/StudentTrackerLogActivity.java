eb2ab250588c5434617e8d5b818898b1c18d2110
Yash Umale <yashumale223@gmail.com>
2021-05-03 11:40:49 +0530

    TrackerLog Activity XML - 5

--- app/src/main/java/com/openlab/humanpokedex/StudentTrackerLogActivity.java
+++ app/src/main/java/com/openlab/humanpokedex/StudentTrackerLogActivity.java
@@ -4,8 +4,17 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class StudentTrackerLogActivity extends AppCompatActivity {
+
+    private RecyclerView.Adapter trackerAdapter;
+    private FirebaseFirestore db;
+    private RecyclerView recyclerView;
+    private SwipeRefreshLayout refreshLayout;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
