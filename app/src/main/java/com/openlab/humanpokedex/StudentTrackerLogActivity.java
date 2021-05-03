21317043d9b2d92758bba12ae96bf3eeb3ec4d52
Yash Umale <yashumale223@gmail.com>
2021-05-03 13:00:40 +0530

    TrackerLog Activity layout manager configured

--- app/src/main/java/com/openlab/humanpokedex/StudentTrackerLogActivity.java
+++ app/src/main/java/com/openlab/humanpokedex/StudentTrackerLogActivity.java
@@ -3,17 +3,20 @@ package com.openlab.humanpokedex;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.vision.Tracker;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
@@ -28,6 +31,8 @@ public class StudentTrackerLogActivity extends AppCompatActivity {
     private ProgressBar progressBar;
     private String regNo;
     private ArrayList<ArrayList<String>> trackerLog;
+    private ArrayList<TrackerLog> trackerLogArray;
+    private TextView trackerLogProgressTV;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -35,9 +40,11 @@ public class StudentTrackerLogActivity extends AppCompatActivity {
         setContentView(R.layout.tracker_log);
         regNo = getIntent().getStringExtra("regNo");
         trackerLog = new ArrayList<>();
+        trackerLogArray = new ArrayList<>();
         db = FirebaseFirestore.getInstance();
         progressBar = findViewById(R.id.trackerLogProgress);
         showTrackerLog();
+        trackerLogProgressTV = findViewById(R.id.trackerLogProgressTV);
 
         refreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
@@ -78,20 +85,22 @@ public class StudentTrackerLogActivity extends AppCompatActivity {
             }
         });
 
-        for (ArrayList<String> log : trackerLog) {
 
+        for (ArrayList<String> log : trackerLog) {
+            trackerLogArray.add(new TrackerLog(log.get(0), log.get(1), log.get(2)));
         }
     }
 
     private void runTrackerUI() {
-        if (classStudents.size() == 0) {
-            classnameTV.setText("Empty Class.");
+        if (trackerLogArray.size() == 0) {
+            trackerLogProgressTV.setText("No track records.");
+            progressBar.setVisibility(View.GONE);
         } else {
-            GridLayoutManager classStudentsLayoutManager = new GridLayoutManager(StudentsClassActivity.this, 3);
-            classStudentsAdapter = new StudentsClassAdapter(classStudents);
+            LinearLayoutManager trackerLayoutManager = new LinearLayoutManager(StudentTrackerLogActivity.this);
+            trackerAdapter = new TrackerAdapter(trackerLogArray);
             recyclerView.setHasFixedSize(true);
-            recyclerView.setLayoutManager(classStudentsLayoutManager);
-            recyclerView.setAdapter(classStudentsAdapter);
+            recyclerView.setLayoutManager(trackerLayoutManager);
+            recyclerView.setAdapter(trackerAdapter);
             // swipeDownRefreshTV.setVisibility(View.GONE);
         }
     }
