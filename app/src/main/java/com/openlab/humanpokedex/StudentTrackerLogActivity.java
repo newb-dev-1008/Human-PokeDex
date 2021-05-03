938139ec5d665f7401772707bc76332978923cb6
Yash Umale <yashumale223@gmail.com>
2021-05-03 11:45:18 +0530

    TrackerLog Activity - 1

--- app/src/main/java/com/openlab/humanpokedex/StudentTrackerLogActivity.java
+++ app/src/main/java/com/openlab/humanpokedex/StudentTrackerLogActivity.java
@@ -1,6 +1,8 @@
 package com.openlab.humanpokedex;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +17,38 @@ public class StudentTrackerLogActivity extends AppCompatActivity {
     private FirebaseFirestore db;
     private RecyclerView recyclerView;
     private SwipeRefreshLayout refreshLayout;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tracker_log);
+        db = FirebaseFirestore.getInstance();
+        progressBar = findViewById(R.id.trackerLogProgress);
+        showTrackerLog();
+
+        refreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                showTrackerLog();
+                refreshLayout.setRefreshing(false);
+            }
+        });
+    }
+
+    private void showTrackerLog() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                backgroundTracker();
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        progressBar.setVisibility(View.VISIBLE);
+                        runTrackerUI();
+                    }
+                });
+            }
+        }).start();
     }
 }
