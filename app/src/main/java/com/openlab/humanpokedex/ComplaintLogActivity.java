ff45fba8a1b36a878502c5df8414d50e56d1e6b9
Yash Umale <yashumale223@gmail.com>
2021-05-03 13:31:58 +0530

    Complaint Log XML

--- app/src/main/java/com/openlab/humanpokedex/ComplaintLogActivity.java
+++ app/src/main/java/com/openlab/humanpokedex/ComplaintLogActivity.java
@@ -2,18 +2,28 @@ package com.openlab.humanpokedex;
 
 import android.os.Bundle;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.ArrayList;
+
 public class ComplaintLogActivity extends AppCompatActivity {
 
-    private RecyclerView recyclerView;
-    private ProgressBar progressBar;
+    private RecyclerView.Adapter complaintAdapter;
     private FirebaseFirestore db;
+    private RecyclerView recyclerView;
+    private SwipeRefreshLayout refreshLayout;
+    private ProgressBar progressBar;
+    private String regNo;
+    private ArrayList<String> complaintLog;
+    private ArrayList<ComplaintLog> trackerLogArray;
+    private TextView complaintLogProgressTV;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
